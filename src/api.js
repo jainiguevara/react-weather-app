62eb0a72d7fbf39b3eae7faf274a912efebc26ee
Jaini Guevara <jaini.guevara@gmail.com>
2019-06-27 12:42:09 +0800

    Update api config

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 const APIID = '1d7c72874c4002fe6394e282d2266ab3';
+const url = 'http://api.openweathermap.org/data/2.5';
 
 export default (uri, city) => {
-  return fetch(`${uri}?q=${city}&APPID=${APIID}`)
+  return fetch(`${url}/${uri}?q=${city}&APPID=${APIID}`)
     .then(response => {
       return response.json()
     })
