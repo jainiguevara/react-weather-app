55b2864ce2540ac7871ebc4bfb4dd467a7c9a371
Jaini Guevara <jaini.guevara@gmail.com>
2019-06-27 14:41:45 +0800

    Cleaned source

--- src/api.js
+++ src/api.js
@@ -1,14 +1,8 @@
 const APIID = '1d7c72874c4002fe6394e282d2266ab3';
-// const url = 'https://api.openweathermap.org/data/2.5'
+const url = "https://api.openweathermap.org/data/2.5";
 
 export default (uri, city) => {
-  
-  const headers = new Headers({
-    'Content-Type': 'application/json',
-  })
-
-  return fetch(`${uri}?q=${city}&APPID=${APIID}`, {
-    headers,
+  return fetch(`${url}${uri}?q=${city}&APPID=${APIID}`, {
     credentials: 'same-origin',
   })
     .then(response => {
